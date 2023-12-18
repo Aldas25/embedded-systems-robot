52e46dc40343c81963658d4724df561cb2e77e07
Aldas25 <aldas.lenksas@gmail.com>
2023-12-18 17:53:24 +0100

    Motors go brrr

--- assignments/1_libopencm3/src/main.c
+++ assignments/1_libopencm3/src/main.c
@@ -7,17 +7,25 @@
 
 int main(void) {
     // Setup
-    rcc_periph_clock_enable(RCC_GPIOC);
+    rcc_periph_clock_enable(RCC_GPIOA);
     rcc_periph_clock_enable(RCC_GPIOB);
-    gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13);
 
-    gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO1);
+    gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8);
+    gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO1 | GPIO3 | GPIO14 | GPIO15);
+
+    //gpio_set(GPIOA, GPIO8);
+    //gpio_set(GPIOB, GPIO3);
+    gpio_set(GPIOB, GPIO14);
+    gpio_clear(GPIOB, GPIO15);
+
+    gpio_set(GPIOA, GPIO8);
+    gpio_clear(GPIOB, GPIO3);
 
     // Main loop
     while (true) {
-      gpio_toggle(GPIOC, GPIO13);
-      gpio_toggle(GPIOB, GPIO1);
-      ms_delay(300U);
+      gpio_toggle(GPIOB, GPIO1); // NOT A motor 
+     // gpio_toggle(GPIOA, GPIO8);
+      ms_delay(1000U);
     }
 
     return 0;
